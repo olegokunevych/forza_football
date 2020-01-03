5f188f05263403bcd83d86ecd36f63ffcf93afaf
Oleg Okunevych <okunevichov@gmail.com>
2020-01-03 15:43:02 +0200

    reduce sql calls

--- lib/forza_assignment/providers/matchbeam/manager.ex
+++ lib/forza_assignment/providers/matchbeam/manager.ex
@@ -4,14 +4,15 @@ defmodule ForzaAssignment.Providers.Matchbeam.Manager do
   @provider_title "Matchbeam"
 
   def call do
+    provider_id = Common.provider_id_by_title(@provider_title)
     ForzaAssignment.Utils.Fetch.call(@url)
     |> Flow.from_enumerable()
     |> Flow.partition()
-    |> Flow.each(fn match -> match |> prepare_match_object |> Common.persist end)
+    |> Flow.each(fn match -> match |> prepare_match_object(provider_id) |> Common.persist end)
     |> Enum.to_list
   end
 
-  defp prepare_match_object(match) do
+  defp prepare_match_object(match, provider_id) do
     [home_team_title, away_team_title] = teams(match)
     {:ok, home_team} = Common.find_or_create_team(home_team_title)
     {:ok, away_team} = Common.find_or_create_team(away_team_title)
@@ -19,7 +20,7 @@ defmodule ForzaAssignment.Providers.Matchbeam.Manager do
     created_at = Common.created_at(match)
 
     %ForzaAssignment.Match{
-      provider_id: Common.provider_id_by_title(@provider_title),
+      provider_id: provider_id,
       home_team_id: home_team.id,
       away_team_id: away_team.id,
       kickoff_at: kickoff_at,
