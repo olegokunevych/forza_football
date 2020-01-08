1d7ca24fbe22f28b21decb1ea1f4ae3f7678df26
Oleg Okunevych <okunevichov@gmail.com>
2020-01-08 16:43:04 +0200

    Add postgres default host for test env

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,7 @@ config :forza_assignment, ForzaAssignment.Repo,
   database: "forza_assignment_repo_test",
   username: "postgres",
   password: "",
-  # hostname: "postgres",
+  hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :logger,
